e4c129a2be70e70e79e99915f6f70552c5dbd7cb
xiaowen <z.xiaowen@outlook.com>
2017-05-10 13:27:45 +0800

    updated assemblyinfo and add xmlnsDefinition updated assembly name to CodeStacks prefix add popwindow project add wpf for xiaowen.codestacks.wpf and wpf is main entry

--- DependencyProperties/ListBoxWithCommand.cs
+++ DependencyProperties/ListBoxWithCommand.cs
@@ -1,12 +1,155 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
-namespace DependencyProperties
+namespace xiaowen.codestacks.dependencyproperties
 {
-    public class ListBoxWithCommand
+    public class ListBoxWithCommand : ListBox, ICommandSource
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public ListBoxWithCommand() : base()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static EventHandler _canExecuteChangedHandler;
+
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register(
+                "Command",
+                typeof(ICommand),
+                typeof(ListBoxWithCommand),
+                new PropertyMetadata((ICommand)null, new PropertyChangedCallback(CommandChanged)));
+
+        public static readonly DependencyProperty CommandTargetProperty = DependencyProperty.Register(
+            "CommandTarget",
+            typeof(object),
+            typeof(ListBoxWithCommand),
+            new PropertyMetadata((object)null)
+            );
+
+        public static readonly DependencyProperty CommandParameterProperty = DependencyProperty.Register(
+            "CommandParameter",
+            typeof(IInputElement),
+            typeof(ListBoxWithCommand),
+            new PropertyMetadata((IInputElement)null)
+            );
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dObject"></param>
+        /// <param name="e"></param>
+        private static void CommandChanged(DependencyObject dObject, DependencyPropertyChangedEventArgs e)
+        {
+            ListBoxWithCommand listbox = (ListBoxWithCommand)dObject;
+            listbox.HookUpCommand((ICommand)e.OldValue, (ICommand)e.NewValue);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="oldCommand"></param>
+        /// <param name="newCommand"></param>
+        private void HookUpCommand(ICommand oldCommand, ICommand newCommand)
+        {
+            if (oldCommand != null)
+            {
+                EventHandler handler = CanExecuteChanged;
+                oldCommand.CanExecuteChanged -= handler;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CanExecuteChanged(object sender, EventArgs e)
+        {
+            if (this.Command != null)
+            {
+                RoutedCommand command = this.Command as RoutedCommand;
+                if (command != null)
+                {
+                    if (command.CanExecute(this.CommandParameter, this.CommandTarget))
+                    {
+                        if (command != null)
+                        {
+                            if (command.CanExecute(this.CommandParameter, this.CommandTarget))
+                                this.IsEnabled = true;
+                            else
+                                this.IsEnabled = false;
+                        }
+                    }
+                }
+                else
+                {
+                    if (this.Command.CanExecute(CommandParameter))
+                        this.IsEnabled = true;
+                    else
+                        this.IsEnabled = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// register command
+        /// </summary>
+        /// <param name="oldCommand"></param>
+        /// <param name="newCommand"></param>
+        private void AddCommand(ICommand oldCommand, ICommand newCommand)
+        {
+            EventHandler handler = new EventHandler(CanExecuteChanged);
+            _canExecuteChangedHandler = handler;
+            if (newCommand != null)
+                newCommand.CanExecuteChanged += _canExecuteChangedHandler;
+        }
+
+        /// <summary>
+        /// Cancellation command
+        /// </summary>
+        /// <param name="oldCommand"></param>
+        /// <param name="newCommand"></param>
+        private void RemoveCommand(ICommand oldCommand, ICommand newCommand)
+        {
+            EventHandler handler = CanExecuteChanged;
+            oldCommand.CanExecuteChanged -= handler;
+        }
+
+        /// <summary>
+        /// from ICommandSource
+        /// </summary>
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        /// <summary>
+        /// from ICommandSource
+        /// </summary>
+        public object CommandParameter
+        {
+            get { return (object)GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        /// <summary>
+        /// from ICommandSource
+        /// </summary>
+        public IInputElement CommandTarget
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
     }
 }
--- PopWindowSolution/Properties/Settings.Designer.cs
+++ PopWindowSolution/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace xiaowen.codestacks.dependencyproperties.Properties {
+namespace xiaowen.codestacks.popwindow.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
