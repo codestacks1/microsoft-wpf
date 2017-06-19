6138d5f818bccc03658e69718bb9f2c48280de39
zhangxiaowen <zhangxiaowen@sensingtech.com.cn>
2017-06-19 13:23:26 +0800

    ...

---
+++ CodeStacks.Data/Models/Enumerate/ErrorNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xiaowen.codestacks.data.Models.Enumerate
+{
+    public enum ErrorNumber
+    {
+        SUCESS = 1,
+        NOTFOUND = 0,
+        EXCEPTION = -1
+    }
+}
